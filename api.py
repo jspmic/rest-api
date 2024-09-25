ce485e3547151417cb388bf9a867e4c7ed4126b0
Jaspe Michael <micaeljaspe@gmail.com>
2024-09-25 08:58:17 +0200

    Added fields and arguments

--- api.py
+++ api.py
@@ -1,10 +1,15 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.dialects.mysql import JSON
+from flask_restful import Resource, Api, reqparse, fields, \
+        marshal_with, abort
+from datetime import datetime
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "mysql:///database.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = \
+        "mysql://micael:micael@localhost/my_database"
 db = SQLAlchemy(app)
+api = Api(app)
 
 
 class Transfert(db.Model):
@@ -20,7 +25,10 @@ class Transfert(db.Model):
 
     stock_central_suivants = db.Column(JSON)
 
-    photo_mvt = db.Column(db.URL, unique=False, nullable=False)
+    stock_central_retour = db.Column(db.String(40), unique=False,
+                                     nullable=False)
+
+    photo_mvt = db.Column(db.String(60), unique=False, nullable=False)
 
     type_transport = db.Column(db.String(25), unique=False, nullable=False)
 
@@ -28,6 +36,49 @@ class Transfert(db.Model):
 
     def __repr__(self):
         return "<Transfert> model"
+
+
+user_args = reqparse.RequestParser()
+user_args.add_argument("date", type=datetime, required=True,
+                       help="<date> cannot be blank")
+user_args.add_argument("plaque", type=str, required=True,
+                       help="<plaque> cannot be blank")
+user_args.add_argument("logistic_official", type=str, required=True,
+                       help="<logistic_official> cannot be blank")
+user_args.add_argument("numero_mouvement", type=int, required=True,
+                       help="<numero_mouvement> cannot be blank")
+user_args.add_argument("stock_depart", type=str, required=True,
+                       help="<stock_depart> cannot be blank")
+user_args.add_argument("stock_suivants", type=JSON, required=True,
+                       help="<stock_suivants> cannot be blank")
+user_args.add_argument("stock_retour", type=str, required=True,
+                       help="<stock_retour> cannot be blank")
+user_args.add_argument("photo_mvt", type=str, required=True,
+                       help="<photo_mvt> cannot be blank")
+user_args.add_argument("type_transport", type=str, required=True,
+                       help="<type_transport> cannot be blank")
+
+transfertFields = {
+    "date": fields.DateTime,
+    "plaque": fields.String,
+    "logistic_official": fields.String,
+    "numero_mouvement": fields.Integer,
+    "stock_depart": fields.String,
+    "stock_suivants": fields.Raw,
+    "stock_retour": fields.String,
+    "photo_mvt": fields.String,
+    "type_transport": fields.String,
+    "motif": fields.String
+}
+
+
+class Transferts(Resource):
+    def get(self):
+        transferts = Transfert.query.all()
+        return transferts
+
+
+api.add_resource(Transferts, "/api/transferts")
 
 
 @app.route("/")

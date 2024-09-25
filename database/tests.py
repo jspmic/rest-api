3ff09eb4a92a062c78772142a363c25aafeafb3d
Jaspe Michael <micaeljaspe@gmail.com>
2024-09-25 20:29:52 +0200

    Deleted manager.py and left tests for the sheets

--- database/tests.py
+++ database/tests.py
@@ -1,44 +1,5 @@
 import unittest
-import manager
 import retrieve
-import mysql.connector as mysql
-
-
-DB_ERRORS = mysql.errors
-
-
-class TestApp(unittest.TestCase):
-    def setUp(self):
-        self.app = manager.App
-
-    def test_connection(self):
-        with self.assertRaises(DB_ERRORS.DatabaseError):
-            # If invalid values are provided to the connector
-            self.app("", "", "", "")
-
-    def test_cursor(self):
-        self.assertEqual(self.app.execute_cmd(self, ""), manager._CURSOR_ERROR)
-
-
-class TestManager(unittest.TestCase):
-    def setUp(self):
-        self._manager = manager
-        self.manager = manager.Manager
-
-    def test_num_operations(self):
-        self._manager.Insert_mvt(self._manager.App,
-                                 self._manager.Manager).register()
-
-        self._manager.View_mvt(self._manager.App,
-                               self._manager.Manager).register()
-
-        self._manager.Create_tables(self._manager.App,
-                                    self._manager.Manager).register()
-
-        self._manager.View_tables(self._manager.App,
-                                  self._manager.Manager).register()
-
-        self.assertEqual(self.manager.num_operations(), 4)
 
 
 class TestWorksheet(unittest.TestCase):
